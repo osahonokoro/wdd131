2a3bb956af0ffa46be4d98c470ed5c1d68748ed4
Osahon Okoro <ookoro2@byupathway.edu>
2025-05-15 14:28:30 +0100

    Initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
