eebe35b03a16d29b7f03c3a0f7419fe56ad0c686
Osahon Okoro <ookoro2@byupathway.edu>
2025-09-14 22:13:01 +0100

    week02 Assignment first commit

---
+++ week02/scripts/blank.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+        input.focus();
+    }
+});
