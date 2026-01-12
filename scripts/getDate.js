fc17441eab1ccc43558ca4712ae4086aeb738e2d
Osahon Okoro <ookoro2@byupathway.edu>
2026-01-12 20:05:47 +0100

    Created Image, styles, and Css folders. then redirected them in index.html file

---
+++ scripts/getDate.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    // Get current year
+    const currentYear = new Date().getFullYear();
+    document.getElementById("year").textContent = currentYear;
+
+    // Get last modified date
+    const lastModified = document.lastModified;
+    document.getElementById("lastModified").textContent = lastModified;
+};
