0824760fa49c3cabc895a162ea3b74d5739cfc59
Osahon Okoro <ookoro2@byupathway.edu>
2026-02-08 12:50:06 +0100

    final corrects to project.html file

--- project/script/getdate.js
+++ project/script/getdate.js
@@ -1,14 +1,9 @@
 // scripts/getdate.js
 
-// Function to update the footer with today's date
 function updateLastModified() {
-  const footerDate = document.getElementById("last-modified");
-  const today = new Date();
-  
-  // Format: Month Day, Year
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  footerDate.textContent = today.toLocaleDateString("en-US", options);
+    const footerDate = document.getElementById("last-modified");
+    const today = new Date();
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    footerDate.textContent = today.toLocaleDateString("en-US", options);
 }
-
-// Run when the page loads
 document.addEventListener("DOMContentLoaded", updateLastModified);
